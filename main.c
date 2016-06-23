ecee3f613fadc29d27ef938dac9f187fa9787f06
Jean-Loïc <jeanloicmugnier@hotmail.com>
2016-06-22 22:14:00 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Jean-Loïc Mugnier <mugnier at polytech.unice.fr>
+ *
+ * Created on June 22, 2016, 9:47 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
